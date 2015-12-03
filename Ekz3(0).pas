83b7d51a87734d8fcba1677b7eb52cea61ada5b6
vladiq <5548529@gmail.com>
2015-12-03 21:58:08 +0300

    add file

---
+++ Ekz3(0).pas
@@ -0,0 +1,49 @@
+program a3;
+
+function maxStep(var a: array of integer): integer;
+var
+  i, j: integer;   
+begin
+  j := 1;
+  result := 0;
+  for i := 0 to length(a) - 2 do 
+  begin
+    if (a[i] < a[i + 1]) then
+    begin
+      j := j + 1;
+    end else
+    begin
+      if (j > result) then
+      begin
+        result := j;
+      end;
+      j := 1;
+    end;
+  end;
+end;
+
+procedure readArray(var a: array of integer);
+var
+  i,n: integer;
+begin
+  write('Enter length of array: ');
+  readln(n);
+  setlength(a,n);
+  for i := 0 to n - 2 do
+  begin
+    read(a[i]);
+  end;
+  a[n-1] := 0;
+  writeln(a[n-1]);
+end;
+
+const
+  n = 100;
+
+var
+  a: array of integer;
+
+begin
+  readArray(a);
+  write('Maximum length of step: ',maxStep(a));
+end.
